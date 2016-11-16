7dc922a1a7b0881da83c1e5c5c46fc5617bf94c4
santiago martini <santiim@hotmail.com>
2016-11-16 15:17:50 -0300

    angular2-angular2-jwt-

---
+++ client/app/app.module.ts
@@ -0,0 +1,38 @@
+import { NgModule }      from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule }    from '@angular/forms';
+import { HttpModule }     from '@angular/http';
+
+import { AppComponent }  from './app.component';
+import { routing }       from './app.routing';
+
+import { HeroesComponent }      from './components/heroes/heroes.component';
+import { DashboardComponent }   from './components/dashboard/dashboard.component';
+import { HeroDetailComponent }  from './components/heroDetail/hero-detail.component';
+
+import { HeroService }  from './services/hero.service';
+
+
+import { AUTH_PROVIDERS } from 'angular2-jwt';
+import { AuthGuard } from './common/auth.guard';
+
+@NgModule({
+  imports: [
+    BrowserModule,
+    HttpModule,
+    FormsModule,
+    routing
+    ],
+  declarations: [
+    AppComponent,
+    HeroesComponent,
+    DashboardComponent,
+    HeroDetailComponent
+  ],
+  providers: [
+    HeroService
+    , AuthGuard, ...AUTH_PROVIDERS
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
